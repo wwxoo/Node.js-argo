5b150cac6f36b9041c3e027f94e6cc826d5eb8ee
Okka <142894633+eoovve@users.noreply.github.com>
2023-10-20 20:50:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const projectPageURL = `https://www.google.com`;// 替换为你的项目域名
 const port = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
-var exec = require("child_process").exec;
+const exec = require("child_process").exec;
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const fs = require("fs");
 const request = require("request");
@@ -13,11 +13,12 @@ const auth = require("basic-auth");
 const axios = require('axios');
 const os = require('os');
 
+
 app.get("/", function(req, res) {
   res.send("hello world");
 });
 
-// 读取 list.txt 文件
+// 读取list和sub文件
 app.get("/list", (req, res) => {
   const user = auth(req);
   if (
@@ -29,6 +30,26 @@ app.get("/list", (req, res) => {
       if (err) {
         console.error(err);
         res.status(500).json({ error: "Error reading list.txt" });
+      } else {
+        res.status(200).send(data);
+      }
+    });
+  } else {
+    res.set("WWW-Authenticate", 'Basic realm="Node"');
+    res.status(401).send("Unauthorized");
+  }
+});
+app.get("/sub", (req, res) => {
+  const user = auth(req);
+  if (
+    user &&
+    user.name === username &&
+    user.pass === password
+  ) {
+    fs.readFile("list.txt", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: "Error reading sub.txt" });
       } else {
         res.status(200).send(data);
       }
@@ -70,14 +91,32 @@ function downloadFile(fileName, fileUrl, callback) {
     .pipe(stream)
     .on("close", function(err) {
       if (err) {
-        callback(`Download${fileName}flie failed`);
+        callback(`Download ${fileName} file failed`);
       } else {
         callback(null, fileName);
       }
     });
 }
 
-function downloadFiles() {
+// 根据系统架构返回对应的文件url
+function getFilesForArchitecture(architecture) {
+  if (architecture === 'arm') {
+    return [
+      { fileName: "web", fileUrl: "https://github.com/eoovve/test/releases/download/ARM/web" },
+      { fileName: "swith", fileUrl: "https://github.com/eoovve/test/releases/download/ARM/swith" },
+      { fileName: "server", fileUrl: "https://github.com/eoovve/test/releases/download/ARM/server" },
+    ];
+  } else if (architecture === 'amd') {
+    return [
+      { fileName: "web", fileUrl: "https://github.com/eoovve/test/raw/main/web" },
+      { fileName: "swith", fileUrl: "https://github.com/eoovve/test/raw/main/swith" },
+      { fileName: "server", fileUrl: "https://github.com/eoovve/test/raw/main/server" },
+    ];
+  }
+  return [];
+}
+
+function downloadAndRunFiles() {
   const architecture = getSystemArchitecture();
   const filesToDownload = getFilesForArchitecture(architecture);
 
@@ -100,44 +139,25 @@ function downloadFiles() {
 
       if (downloadedCount === filesToDownload.length) {
         console.log("All files downloaded");
+
+        // 执行start.sh
+        exec("bash start.sh", function(err, stdout, stderr) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          console.log(stdout);
+        });
+
+        app.listen(port, () => console.log(`Server is running on port ${port}!`));
       }
     });
   });
 }
-
-// 根据系统架构返回对应的文件url
-function getFilesForArchitecture(architecture) {
-  if (architecture === 'arm') {
-    return [
-      { fileName: "web", fileUrl: "https://github.com/eoovve/test/releases/download/ARM/web" },
-      { fileName: "swith", fileUrl: "https://github.com/eoovve/test/releases/download/ARM/swith" },
-      { fileName: "server", fileUrl: "https://github.com/eoovve/test/releases/download/ARM/server" },
-    ];
-  } else if (architecture === 'amd') {
-    return [
-      { fileName: "web", fileUrl: "https://github.com/eoovve/test/raw/main/web" },
-      { fileName: "swith", fileUrl: "https://github.com/eoovve/test/raw/main/swith" },
-      { fileName: "server", fileUrl: "https://github.com/eoovve/test/raw/main/server" },
-    ];
-  }
-  return [];
-}
-downloadFiles();
-
-exec("bash start.sh", function(err, stdout, stderr) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(stdout);
-});
-
-app.listen(port, () => console.log(`Server is running on port ${port}!`));
-
+downloadAndRunFiles();
 
 // 定义访问间隔时间（2分钟）
 const intervalInMilliseconds = 2 * 60 * 1000;
-
 async function visitProjectPage() {
   try {
     // console.log(`Visiting project page: ${projectPageURL}`);
