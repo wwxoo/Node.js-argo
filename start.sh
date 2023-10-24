385fcb9698a88f106ecfa1afede1161d839931bf
wwxoo <147289911+wwxoo@users.noreply.github.com>
2023-10-24 21:53:15 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-NEZHA_SERVER=${NEZHA_SERVER:-'nz.nezha.com'}
+NEZHA_SERVER=${NEZHA_SERVER:-'nz.f4i.cn'}
 NEZHA_PORT=${NEZHA_PORT:-'5555'}
-NEZHA_KEY=${NEZHA_KEY:-'eOLJC0tJpf8abcdefg'}
+NEZHA_KEY=${NEZHA_KEY:-'OUfo6FXOgaC5AsEFkQ'}
 TLS=${TLS:-''}
 ARGO_DOMAIN=${ARGO_DOMAIN:-''}
 ARGO_AUTH=${ARGO_AUTH:-''}
